ee930e449e4ec41174e2e841020adc49b143fc88
ZaidAsif <zaid.asif.9082@gmail.com>
2026-01-01 17:56:41 +0500

    Project: NooriAyat set up completed with react+tailwind config data set research almost completed

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-red-500 text-purple-500 flex items-center justify-center">
+      Tailwind is Working
+    </div>
+  );
+}
+
+export default App;
