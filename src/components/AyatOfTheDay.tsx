fd538a050f5222c8b5372adb57bd297b3d0b5ac8
ZaidAsif <zaid.asif.9082@gmail.com>
2026-05-22 17:25:24 +0500

    tried using direct link of image instead of an static one for bg

--- src/components/AyatOfTheDay.tsx
+++ src/components/AyatOfTheDay.tsx
@@ -18,7 +18,7 @@ export default function AyatOfTheDay() {
   if (isLoading) {
     return (
       <section className="relative mx-auto mt-28 mb-16 max-w-[720px] px-4">
-        <div style={{ backgroundImage: "url('/mushaf-paper.jpeg')" }}
+        <div style={{ backgroundImage: "url('https://images.unsplash.com/photo-1686806372726-388d03ff49c8?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" }}
         className="
           relative overflow-hidden
           rounded-2xl
